a3272e8d6762526d04a4abce214fa7892fd74101
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-17 18:26:33 +0200

    ISSUE #4 * Improve the `order` microservice.

--- tests/order/aggregates.py
+++ tests/order/aggregates.py
@@ -5,12 +5,14 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .services import (
-    CommandService,
-    CqrsService,
-    RestService,
-    SagaService,
+
+from minos.common import (
+    Aggregate,
 )
-from .aggregates import (
-    Order,
-)
+
+
+class Order(Aggregate):
+    """Aggregate ``Order`` class for testing purposes."""
+
+    product: str
+    amount: int
