0565be579a15fe0b4d8d26d8f43cb50f7b2a0d6f
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-27 16:01:37 +0200

    ISSUE #4 * Minor change...

--- tests/order/services/saga.py
+++ tests/order/services/saga.py
@@ -26,12 +26,6 @@ class Bar(MinosModel):
     bar: str
 
 
-class FooBar(MinosModel):
-    """Utility minos model class for testing purposes"""
-
-    bar: str
-
-
 # noinspection PyUnusedLocal
 def foo_fn(context: SagaContext) -> MinosModel:
     """Utility callback function for testing purposes.
@@ -55,12 +49,6 @@ def finish_fn_2(order):
 
 
 # noinspection PyUnusedLocal
-def finish_fn_3(order):
-    """Utility callback function for testing purposes."""
-    return FooBar("finish")
-
-
-# noinspection PyUnusedLocal
 def finish_fn_raises(order):
     """Utility callback function for testing purposes."""
     raise ValueError()
@@ -76,7 +64,6 @@ class SagaService(object):
         .step()
         .invoke_participant("AddOrder", foo_fn)
         .with_compensation("DeleteOrder", foo_fn)
-        .on_reply("order2", finish_fn_2)
         .step()
         .invoke_participant("AddOrder", foo_fn)
         .with_compensation("DeleteOrder", foo_fn)
@@ -84,6 +71,6 @@ class SagaService(object):
         .step()
         .invoke_participant("AddOrder", foo_fn)
         .with_compensation("DeleteOrder", foo_fn)
-        .on_reply("order4", finish_fn_3)
+        .on_reply("order4", finish_fn_2)
         .commit()
     )
