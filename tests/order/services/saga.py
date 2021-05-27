d4c1cf9aada471f5f0c37cee5f1ed5102203c7cc
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-27 10:28:24 +0200

    ISSUE #4 * Minor change.

--- tests/order/services/saga.py
+++ tests/order/services/saga.py
@@ -20,6 +20,18 @@ class Foo(MinosModel):
     foo: str
 
 
+class Bar(MinosModel):
+    """Utility minos model class for testing purposes"""
+
+    bar: str
+
+
+class FooBar(MinosModel):
+    """Utility minos model class for testing purposes"""
+
+    bar: str
+
+
 # noinspection PyUnusedLocal
 def foo_fn(context: SagaContext) -> MinosModel:
     """Utility callback function for testing purposes.
@@ -30,8 +42,28 @@ def foo_fn(context: SagaContext) -> MinosModel:
     return Foo("hello")
 
 
-def finish_fn(order):
+# noinspection PyUnusedLocal
+def finish_fn_1(order):
+    """Utility callback function for testing purposes."""
     return Foo("finish")
+
+
+# noinspection PyUnusedLocal
+def finish_fn_2(order):
+    """Utility callback function for testing purposes."""
+    return Bar("finish")
+
+
+# noinspection PyUnusedLocal
+def finish_fn_3(order):
+    """Utility callback function for testing purposes."""
+    return FooBar("finish")
+
+
+# noinspection PyUnusedLocal
+def finish_fn_raises(order):
+    """Utility callback function for testing purposes."""
+    raise ValueError()
 
 
 class SagaService(object):
@@ -39,9 +71,19 @@ class SagaService(object):
         Saga("UpdateOrder")
         .step()
         .invoke_participant("AddOrder", foo_fn)
-        .on_reply("order1", finish_fn)
+        .with_compensation("DeleteOrder", foo_fn)
+        .on_reply("order1", finish_fn_1)
         .step()
-        .invoke_participant("DeleteOrder", foo_fn)
-        .on_reply("order2", finish_fn)
+        .invoke_participant("AddOrder", foo_fn)
+        .with_compensation("DeleteOrder", foo_fn)
+        .on_reply("order2", finish_fn_2)
+        .step()
+        .invoke_participant("AddOrder", foo_fn)
+        .with_compensation("DeleteOrder", foo_fn)
+        .on_reply("order3", finish_fn_raises)
+        .step()
+        .invoke_participant("AddOrder", foo_fn)
+        .with_compensation("DeleteOrder", foo_fn)
+        .on_reply("order4", finish_fn_3)
         .commit()
     )
